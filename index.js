69f08012919824020999cb8da123597bd839c851
Thiago Meireles <thiago22md@gmail.com>
2023-12-12 13:29:29 -0300

    changeDirection

--- index.js
+++ index.js
@@ -25,3 +25,18 @@ const handleGameOver = () => {
     location.reload();
 }
 
+const changeDirection = e => {
+    if (e.key === "ArrowUp" && velocityY != 1) {
+        velocityX = 0;
+        velocityY = -1;
+    } else if (e.key === "ArrowDown" && velocityY != -1) {
+        velocityX = 0;
+        velocityY = 1;
+    } else if (e.key === "ArrowLeft" && velocityX != 1) {
+        velocityX = -1;
+        velocityY = 0;
+    } else if (e.key === "ArrowRight" && velocityX != -1) {
+        velocityX = 1;
+        velocityY = 0;
+    }
+}
